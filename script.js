25ca11771ca5e2588fb154af7508f416edf2b888
adejumo idris <100483288+adejumoisris@users.noreply.github.com>
2022-06-19 13:26:10 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increament () {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    console.log(count)
+}
+
+let saveEl = document.getElementById("save-el")
+
+let count = 1 
+
+console.log(saveEl)
+function save() {
+    count +=12
+    saveEl.innerText = count
+    count. textContent = 0
+}
+
+
+let saveEl = document.getElementById("save-el")
+
+console.log(saveEl)
+
+function save () {
+    let countstr = count + ;"-"
+    saveEl.innerText = countstr
+
+    console.log(count)
+}
